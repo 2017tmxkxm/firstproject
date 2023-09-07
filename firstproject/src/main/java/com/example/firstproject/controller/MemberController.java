d4a8b3cf19675630a3323131e8ed9de3514a6af6
2017tmxkxm <2017tmxkxm@gmail.com>
2023-09-07 14:54:07 +0900

    lombok 설치 및 코드 리펙토링(dto, entity, controller)

--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -3,11 +3,13 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.MemberForm;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class MemberController {
 
@@ -23,8 +25,10 @@ public class MemberController {
     public String join(MemberForm memberForm) {
 
         Member member = memberForm.toEntity();
+        log.info(member.toString());
 
         Member saved = memberRepository.save(member);
+        log.info(saved.toString());
 
         return "";
     }
--- firstproject/src/main/java/com/example/firstproject/entity/Member.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Member.java
@@ -5,8 +5,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@ToString
 public class Member {
 
     @Id
@@ -19,9 +23,4 @@ public class Member {
     @Column
     private String password;
 
-    public Member(Long id, String email, String password) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-    }
 }
