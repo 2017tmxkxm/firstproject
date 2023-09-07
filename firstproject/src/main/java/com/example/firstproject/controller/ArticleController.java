5b912214277efc8450096d94f66122bc3b061f27
2017tmxkxm <2017tmxkxm@gmail.com>
2023-09-07 15:52:47 +0900

    게시판 Read

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -31,5 +33,15 @@ public class ArticleController {
         log.info(saved.toString());
 
         return "";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model) {
+
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        model.addAttribute("article", articleEntity);
+
+        return "articles/show";
     }
 }
