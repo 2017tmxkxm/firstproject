83e23ea500153ea48e7fd3c3cae6e74992e0d09b
2017tmxkxm <2017tmxkxm@gmail.com>
2023-09-08 14:53:36 +0900

    article, member delete 추가

--- firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -78,6 +79,19 @@ public class MemberController {
         }
 
         return "redirect:/members/" + memberEntity.getId();
+    }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+
+        Member target = memberRepository.findById(id).orElse(null);
+
+        if(target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제되었습니다!");
+        }
+
+        return "redirect:/member";
     }
 
 }
