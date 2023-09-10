3b90573b74220ec8ad604fca579618ed06c4756e
2017tmxkxm <2017tmxkxm@gmail.com>
2023-09-10 19:46:57 +0900

    test 코드 추가 (select, create)

---
+++ firstproject/src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -0,0 +1,86 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ArticleServiceTest {
+
+    @Autowired
+    ArticleService articleService;
+
+    @Test
+    void index() {
+        Article a = new Article(1L, "제목1", "내용1");
+        Article b = new Article(2L, "제목2", "내용2");
+        Article c = new Article(3L, "제목3", "내용3");
+        List<Article> expected = new ArrayList<Article>(Arrays.asList(a,b,c));
+
+        List<Article> articles = articleService.index();
+
+        assertEquals(expected.toString(), articles.toString());
+    }
+
+    @Test
+    void show_성공_존재하는_id_입력() {
+        Long id = 1L;
+        Article expected = new Article(id, "제목1", "내용1");
+
+        Article article = articleService.show(id);
+
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    void show_실패_존재하지_않는_id_입력() {
+        Long id = -1L;
+        Article expected = null;
+
+        Article article = articleService.show(id);
+
+        assertEquals(expected, article);
+
+    }
+
+    @Test
+    @Transactional
+    void create_성공_title과_content만_있는_dto_입력() {
+        // 예상 데이터
+        String title = "제목4";
+        String content = "내용4";
+        Article expected = new Article(4L, title, content);
+
+        // 실제 데이터
+        ArticleForm dto = new ArticleForm(null,title,content);
+        Article article = articleService.create(dto);
+
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    @Transactional
+    void create_실패_id가_포함된_dto_입력() {
+        // 예상 데이터
+        Article expected = null;
+
+        // 실제 데이터
+        Long id = 4L;
+        String title = "제목4";
+        String content = "내용4";
+
+        ArticleForm dto = new ArticleForm(id, title, content);
+        Article article = articleService.create(dto);
+
+        assertEquals(expected, article);
+    }
+}
